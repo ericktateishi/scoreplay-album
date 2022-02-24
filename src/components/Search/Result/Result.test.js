4d3921101021669c0c8fb59e701c8ba7389ed567
erick at trio <ericktateishi@gmail.com>
2022-02-24 17:58:40 +0000

    FIX: result test, missing correct redux render

--- src/components/Search/Result/Result.test.js
+++ src/components/Search/Result/Result.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { cleanup, render } from "@testing-library/react"
+import { cleanup, render } from "test/setup"
 import Result from "."
 
 afterEach(cleanup)
